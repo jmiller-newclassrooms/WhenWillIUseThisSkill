0d819efb134f340df2d2a19a35caa2a1efa05556
(S)am <sam_saarinen@alumni.brown.edu>
2023-09-29 15:46:22 -0400

    Generate pages from JSON.

---
+++ use-cases.js
@@ -0,0 +1,321 @@
+export const useCases = [
+  {
+    "skill": 101,
+    "name": "Multiply & Divide by Powers of 10",
+    "examples": [
+      {
+        "title": "Astrobiology",
+        "type": "serious",
+        "content": "When studying extremophiles (organisms that can live in extreme environments) on other planets, scientists may need to convert temperatures from Celsius to Kelvin (multiply by 1.0) or back to Celsius (divide by 1.0) to understand the habitability of those environments."
+      },
+      {
+        "title": "Forensic Entomology",
+        "type": "serious",
+        "content": "Forensic entomologists use the growth rate of insects on a corpse to estimate the time of death. They might need to multiply or divide by powers of 10 when converting between hours, days, and weeks."
+      },
+      {
+        "title": "Cryptography",
+        "type": "serious",
+        "content": "Cryptographers use large prime numbers to create secure encryption. They may need to multiply and divide these numbers by powers of 10 to manage the complexity of their algorithms."
+      },
+      {
+        "title": "Ethnobotany",
+        "type": "serious",
+        "content": "Ethnobotanists study traditional plant knowledge. They may need to convert measurements of medicinal plant compounds from grams to milligrams (multiply by 1,000) for precise analysis."
+      },
+      {
+        "title": "Marine Archaeology",
+        "type": "serious",
+        "content": "When exploring shipwrecks in deep-sea environments, marine archaeologists use remotely operated vehicles (ROVs) with cameras. They might need to convert depths from meters to kilometers (multiply by 0.001) to map the site accurately."
+      }
+    ]
+  },
+
+  {
+    "skill": 144,
+    "name": "Coordinate Plane in 4 Quadrants",
+    "examples": [
+        {
+            "title": "Dance Choreography",
+            "type": "serious",
+            "content": "When creating complex dance routines, choreographers use the coordinate plane to plan movements precisely. For example, they might need to calculate how many units a dancer should move on the x and y axes to go from point (0, 0) to point (5, 7) in a certain number of steps."
+        },
+        {
+            "title": "Environmental Conservation",
+            "type": "serious",
+            "content": "Environmental scientists use the coordinate plane to track wildlife habitats. They may analyze the coordinates of animals and calculate distances and speeds, such as determining the average speed of a tiger based on its coordinates over time."
+        },
+        {
+            "title": "Astrobiology",
+            "type": "serious",
+            "content": "When studying extremophiles (organisms that can live in extreme environments) on other planets, scientists may need to convert temperatures from Celsius to Kelvin (multiply by 1.0) or back to Celsius (divide by 1.0) to understand the habitability of those environments."
+        },
+        {
+            "title": "Architectural Design",
+            "type": "serious",
+            "content": "Architects use coordinates to design structures. They could map out the floor plan of a house, ensuring rooms are accurately positioned."
+        },
+        {
+            "title": "Music Production",
+            "type": "serious",
+            "content": "Music producers use coordinates to compose electronic music. They might plot notes on a grid to create melodies and harmonies."
+        }
+    ]
+  },
+
+  {
+    "skill": 152,
+    "name": "Solve 2-Step Inequalities",
+    "examples": [
+      {
+        "title": "Oceanography",
+        "type": "serious",
+        "content": "Oceanographers study the world's oceans, including their depths, currents, and ecosystems. They use two-step inequalities to determine safe diving depths for submarines, ensuring they descend to the proper depth range."
+      },
+      {
+        "title": "Meteorology",
+        "type": "serious",
+        "content": "Meteorologists study weather patterns, climate, and atmospheric conditions. They use two-step inequalities to forecast temperature ranges, ensuring accurate predictions."
+      },
+      {
+        "title": "Sports Analytics",
+        "type": "serious",
+        "content": "Sports analysts use data to gain insights into athletic performance and strategy. They apply two-step inequalities to evaluate player statistics, ensuring accurate assessments."
+      },
+      {
+        "title": "Interior Design",
+        "type": "serious",
+        "content": "Interior designers plan and decorate indoor spaces. They use two-step inequalities to manage project costs, ensuring that they stay within budget constraints."
+      },
+      {
+        "title": "Environmental Science",
+        "type": "serious",
+        "content": "Environmental scientists study the natural world and its ecosystems. They use two-step inequalities to assess air quality, ensuring that pollutant concentrations are within acceptable ranges."
+      }
+    ]
+  },
+
+  {
+    "skill": 185,
+    "name": "Identify Factors of Whole Numbers",
+    "examples": [
+      {
+        "title": "Volcanology",
+        "type": "serious",
+        "content": "Volcanologists study volcanoes and volcanic activity. They use factor analysis to identify common factors contributing to the frequency of volcanic eruptions in a region."
+      },
+      {
+        "title": "Artificial Intelligence (AI)",
+        "type": "serious",
+        "content": "AI engineers develop intelligent computer systems. They use factor analysis to identify factors that affect the performance of machine learning models."
+      },
+      {
+        "title": "Economic Forecasting",
+        "type": "serious",
+        "content": "Economic analysts predict economic trends. They use factor analysis to identify factors that influence economic growth."
+      },
+      {
+        "title": "Historical Linguistics",
+        "type": "serious",
+        "content": "Historical linguists study language evolution. They use factor analysis to identify factors that contributed to language changes in a historical context."
+      },
+      {
+        "title": "Sustainable Agriculture",
+        "type": "serious",
+        "content": "Sustainable agriculture experts promote eco-friendly farming practices. They use factor analysis to identify factors that impact crop growth in a sustainable farming system."
+      }
+    ]
+  },
+
+  {
+    skill: 209, 
+    name: "Subtract Integers", 
+    examples:[
+      {
+        title: "Urban Exploration",
+        type: "serious",
+        content: "Urban explorers investigate abandoned buildings and underground tunnels. They may need to subtract negative values representing the depth of underground passages to calculate their total length."
+      },
+      {
+        title: "Iceberg Research",
+        type: "serious",
+        content: "Researchers studying icebergs need to calculate changes in iceberg depth below sea level. They might need to subtract integers to determine the iceberg's position in the water."
+      },
+      {
+        title: "Music Therapy",
+        type: "serious",
+        content: "Music therapists use music to help patients with emotional and psychological issues. When analyzing mood changes in patients, they might need to subtract negative values representing a decrease in anxiety levels."
+      },
+      {
+        title: "Environmental Soundscaping",
+        type: "serious",
+        content: "Soundscapers create natural sound environments for various purposes, such as stress reduction. They may need to subtract negative values representing sound intensity levels to calculate the overall sound intensity."
+      },
+      {
+        title: "Ghost Hunting",
+        type: "silly",
+        content: "Paranormal investigators use specialized equipment to measure paranormal activity. They might need to subtract integers when analyzing changes in temperature to identify anomalies."
+      },
+    ]
+  },
+
+  {
+    skill: 220,
+    name: "Understand Absolute Value",
+    examples: [
+      {
+        title: "Cryptozoology (Study of Hidden Animals)",
+        type: "serious",
+        content: "Cryptozoologists investigate reports of mysterious creatures, including the depth at which they are believed to live. Understanding absolute value helps them analyze the potential depths of these cryptic animals."
+      },
+      {
+        title: "Ghost Hunting",
+        type: "silly",
+        content: "Ghost hunters use various instruments to measure paranormal activity, including changes in temperature. Absolute value helps them interpret temperature fluctuations, whether they are increases or decreases."
+      },
+      {
+        title: "Ufology (Study of Unidentified Flying Objects)",
+        type: "silly",
+        content: "Ufologists analyze sightings of UFOs, which often involve rapid changes in altitude. Understanding absolute value helps them quantify these changes and assess the capabilities of unidentified flying objects."
+      },
+      {
+        title: "Parapsychology (Study of Psychic Phenomena)",
+        type: "silly",
+        content: "Parapsychologists study psychic abilities, including telekinesis, which can involve both the movement of objects up or down. Absolute value helps them measure the strength of telekinetic forces."
+      },
+      {
+        title: "Exobotany (Study of Extraterrestrial Plants)",
+        type: "serious",
+        content: "Exobotanists explore the possibility of plant life on other planets. Absolute value is useful for interpreting temperature variations on alien worlds, whether they are extreme cold or extreme heat."
+      }
+    ]
+  },
+
+  {
+    "skill": 304,
+    "name": "1-Step Equations Word Problems",
+    "examples": [
+      {
+        "title": "Archaeoastronomy",
+        "type": "serious",
+        "content": "Archaeoastronomers study ancient structures and their alignment with celestial events. To determine the age of a historical site, they might need to solve a one-step equation involving the number of years since a significant celestial event, like a solar eclipse."
+      },
+      {
+        "title": "Ice Cream Chemistry",
+        "type": "silly",
+        "content": "Ice cream chemists create new flavors by adjusting ingredient ratios. They need to calculate the amount of each ingredient needed to make a specific flavor, which involves solving one-step equations."
+      },
+      {
+        "title": "Digital Paleontology",
+        "type": "serious",
+        "content": "Digital paleontologists use 3D modeling to reconstruct fossilized bones. They might need to solve equations to scale down the size of a digital model for printing."
+      },
+      {
+        "title": "Fashion Sustainability",
+        "type": "serious",
+        "content": "Sustainable fashion designers work on reducing waste by using fabric efficiently. They may need to calculate the length of fabric required to make a garment, involving one-step equations."
+      },
+      {
+        "title": "Robot Psychology",
+        "type": "serious",
+        "content": "Robot psychologists study human-robot interactions. They may need to determine how long it takes for a robot to recognize a specific command, which involves solving a one-step equation related to reaction time."
+      }
+    ]
+  },
+
+  {
+    "skill": 524,
+    "name": "Distributive Property",
+    "examples": [
+      {
+        "title": "Archeoacoustics",
+        "type": "serious",
+        "content": "In archeoacoustics (study of sound in archaeology), researchers may need to analyze the acoustic properties of ancient structures. The Distributive Property can help them calculate the total area of sound-reflecting surfaces when they are partitioned into different shapes."
+      },
+      {
+        "title": "Cryonics",
+        "type": "serious",
+        "content": "In cryonics (preservation of bodies at low temperatures), experts need to determine the surface area of complex-shaped cryogenic containers. The Distributive Property helps them calculate the area accurately by breaking it down into simpler geometric shapes."
+      },
+      {
+        "title": "Ergonomics",
+        "type": "serious",
+        "content": "In ergonomics (design of workspaces for human efficiency), specialists analyze the layout of workspace elements like desks and partitions. They use the Distributive Property to distribute dimensions and calculate the total area for efficient workspace design."
+      },
+      {
+        "title": "Music Therapy",
+        "type": "serious",
+        "content": "In music therapy (using music for healing), therapists work with varying musical compositions. The Distributive Property can be applied to expand and simplify musical expressions, making it easier to analyze and modify music for therapeutic purposes."
+      },
+      {
+        "title": "Ethnomathematics",
+        "type": "serious",
+        "content": "In ethnomathematics (mathematics in different cultures), ethnomathematicians explore mathematical concepts in various cultural contexts. They may use the Distributive Property to relate indigenous mathematical techniques to the more familiar concept of the distributive property."
+      }
+    ]
+  },
+
+  {
+    "skill": 632,
+    "name": "Multiply 2-digit Whole Numbers",
+    "examples": [
+      {
+        "title": "Archaeology",
+        "type": "serious",
+        "content": "Archaeologists study ancient civilizations and artifacts. They use multiplication to calculate the area of excavation sites for archaeological digs, ensuring efficient planning."
+      },
+      {
+        "title": "Architecture",
+        "type": "serious",
+        "content": "Architects design buildings and structures. They use multiplication to estimate material quantities for construction projects, ensuring accurate planning."
+      },
+      {
+        "title": "Astronomy",
+        "type": "serious",
+        "content": "Astronomers study celestial objects and the universe. They use multiplication to calculate astronomical distances and the speed of light, contributing to space exploration."
+      },
+      {
+        "title": "Zoology",
+        "type": "serious",
+        "content": "Zoologists study animals and their behavior. They use multiplication to estimate animal populations in wildlife habitats, aiding conservation efforts."
+      },
+      {
+        "title": "Game Development",
+        "type": "serious",
+        "content": "Game developers create video games. They use multiplication to design game levels with grid layouts, ensuring precise placement of game elements."
+      }
+    ]
+  },
+
+  {
+    "skill": 632,
+    "name": "Multiplicative Comparisons",
+    "examples": [
+      {
+        "title": "Archaeoastronomy",
+        "type": "serious",
+        "content": "Archaeoastronomers study ancient cultures' astronomical practices. They use multiplicative comparisons to understand how ancient civilizations calculated lunar cycles for their calendars."
+      },
+      {
+        "title": "Environmental Conservation",
+        "type": "serious",
+        "content": "Conservationists protect ecosystems and wildlife. They use multiplicative comparisons to analyze population growth of endangered species."
+      },
+      {
+        "title": "Fashion Design Technology",
+        "type": "serious",
+        "content": "Fashion technologists create sustainable clothing. They use multiplicative comparisons to calculate the environmental impact of fabric production with different technologies."
+      },
+      {
+        "title": "Space Tourism",
+        "type": "serious",
+        "content": "Space tourism experts plan commercial space travel. They use multiplicative comparisons to estimate cost savings with reusable rocket technology."
+      },
+      {
+        "title": "Music Production",
+        "type": "serious",
+        "content": "Music producers create music with technology. They use multiplicative comparisons to adjust track volumes for sound balance."
+      }
+    ]
+  },
+];
